778758fd1b8512491031aebf2be219337bb103cd
Sameer Khanal <khanal.sam91@gmail.com>
2018-08-20 11:17:28 +1200

    added HeaderComponent

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+const appRoutes: Routes = [
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
