8b3ca3400131f21107c6fb29d6ce72cad156c10b
Sameer Khanal <khanal.sam91@gmail.com>
2018-08-22 12:10:20 +1200

    added basic loading indicator on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showLoadingIndicator = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routerEvent) => {
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
 }
